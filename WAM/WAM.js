6d2f832ac59f28b970c656e4c2d24fb835b50169
Matías Gemignani <63791086+matias56@users.noreply.github.com>
2023-04-21 00:07:54 -0300

    Add files via upload

--- WAM/WAM.js
+++ WAM/WAM.js
@@ -4,4 +4,49 @@ const timeLeft  = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPos
+let currentTime = 60
 
+function randomSquare(){
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+
+    randomSquare.classList.add('mole')
+
+    hitPos = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPos){
+            result++
+            score.textContent = result
+            hitPos = null
+        }
+    })
+})
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown()
+{
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0)
+    {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
